b76b295727e1a47aba5783c37d314ec69743a9ea
Ian Campbell <ian.campbell.92@gmail.com>
2021-01-26 21:15:14 -0700

    needed fields

--- books/serializers.py
+++ books/serializers.py
@@ -3,32 +3,29 @@ from .models import *
 
 from rest_framework.serializers import ModelSerializer
 
+class CategorySerializer(ModelSerializer):
+  class Meta:
+      model = Category
+      fields = ('name',)
 
 class BookSerializer(ModelSerializer):
   class Meta:
       model = Book
-      # fields = ('title','url','author','category')
-
-
-class CategorySerializer(ModelSerializer):
-  class Meta:
-      model = Category
-      # fields = ('name')
-
+      fields = ('title','url','category')
 
 class UserSerializer(ModelSerializer):
   class Meta:
       model = User
-      # fields = ('name','email','password_digest','intro')
+      fields = ('name','email','password_digest','intro')
 
 
 class BookCategorySerializer(ModelSerializer):
   class Meta:
       model = BookCategory
-      # fields = ('book','category','user')
+      fields = ('book','category','user')
 
 
 class UserHistorySerializer(ModelSerializer):
   class Meta:
       model = UserHistory
-      # fields = ('progress','book','user')
+      fields = ('progress','book','user')
